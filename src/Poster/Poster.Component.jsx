368e81ea4a715c066552fb6f520613c141ef7d2f
Shree01@29 <shreerajshinde86@gmail.com>
2023-05-26 11:18:59 +0530

    completed project

--- src/Poster/Poster.Component.jsx
+++ src/Poster/Poster.Component.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Poster = (props) => {
+const MoviePoster = (props) => {
   return (
-    <Link to={`/movie/${props.id}`}>
+    <>
+      <Link to={`/movie/${props.id}`}>
+        <div className="flex flex-col px-1 md:px-3 gap-2">
+          <div className="h-40 md:h-80">
+            <img
+              className="w-full h-full rounded-md "
+              src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
+              alt=""
+            />
+          </div>
+          <h3
+            className={`text-lg text-gray-700 font-bold ${
+              props.isDark ? "text-white" : "text-gray-700"
+            }`}
+          >
+            {props.title}
+          </h3>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+const PlayPoster = (props) => {
+  return (
+    <a
+      href="https://in.bookmyshow.com/events/abhangawari/ET00359489"
+      target="_blank"
+      rel="noreferrer"
+    >
       <div className="flex flex-col px-1 md:px-3 gap-2">
         <div className="h-40 md:h-80">
           <img
-            className="w-full h-full rounded-md"
-            src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
+            className="w-full h-full rounded-md object-cover object-center"
+            src={props.src}
             alt=""
           />
         </div>
@@ -20,8 +49,15 @@ const Poster = (props) => {
           {props.title}
         </h3>
       </div>
-    </Link>
+    </a>
   );
+};
+
+const Poster = (props) => {
+  if (props.isPlay) {
+    return <PlayPoster {...props} />;
+  }
+  return <MoviePoster {...props} />;
 };
 
 export default Poster;
--- src/pages/Plays.page.jsx
+++ src/pages/Plays.page.jsx
@@ -1,8 +1,70 @@
 import React from "react";
 import DefaultLayout from "../Layout/Default.Layout";
+import Poster from "../Poster/Poster.Component";
+import PlayFilter from "../Components/PlayFilters/PlayFilter.Component";
 
 const Playspage = () => {
-  return <div>Playspage</div>;
+  return (
+    <>
+      <div className="container mx-auto px-4 my-10">
+        <div className="w-full flex flex-col-reverse lg:flex lg:flex-row-reverse gap-4 ">
+          <div className="lg:w-3/4 p-4 bg-white rounded">
+            <h2 className="text-2xl font-bold mb-4">Plays in Pune</h2>
+            <div className="flex flex-wrap">
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-abhangawari-by-mahesh-kale-pune-0-2023-5-17-t-11-10-29.jpg"
+                  title=" Abhangawari by Mahesh Kale"
+                  subtitle=" Retro | Marathi | All age groups | 2hrs 30mins"
+                />
+              </div>
+
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-emiway-bantai-live-in-concert-0-2023-5-10-t-4-11-30.jpg"
+                  title=" Abhangawari by Mahesh Kale"
+                  subtitle=" Retro | Marathi | All age groups | 2hrs 30mins"
+                />
+              </div>
+
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-abhangawari-by-mahesh-kale-pune-0-2023-5-17-t-11-10-29.jpg"
+                  title=" Abhangawari by Mahesh Kale"
+                  subtitle=" Retro | Marathi | All age groups | 2hrs 30mins"
+                />
+              </div>
+
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-unfiltered-comedy-show-balewadi-0-2023-2-7-t-4-37-37.jpg"
+                  title=" Unfiltered Comedy Show @ Balewadi"
+                  subtitle=" Retro | Marathi | All age groups | 2hrs 30mins"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/4 p-4 bg-white rounded ">
+            <h2 className="text-2xl font-bold mb-4">Filter</h2>
+            <div className="">
+              <PlayFilter
+                title="Date"
+                tags={["Today", "Tomorrow", "This Weekend"]}
+              />
+              <PlayFilter
+                title="Language"
+                tags={["English", "Hindi", "Marathi"]}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DefaultLayout(Playspage);
